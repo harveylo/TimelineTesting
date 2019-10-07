5bc7436c3f872760eed2c722f8bee0304182a71d
ceej <ceej_7@hotmail.com>
2019-10-08 00:30:59 +0800

    test: testing feature

--- src/test/java/controller/ArticleControllerTest.java
+++ src/test/java/controller/ArticleControllerTest.java
@@ -1,7 +1,19 @@
 package controller;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class ArticleControllerTest {
+    DataBaseUtility mock_dbUtil;
+
+    @BeforeAll
+    public void mocksetup(){
+        mock_dbUtil = mock(DataBaseUtility.class);
+    }
+
+    @Test
+    @DisplayName("user")
 
 }
