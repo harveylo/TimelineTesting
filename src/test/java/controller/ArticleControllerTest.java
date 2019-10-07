0eaa18d3f85826bf038c6951345671a8ca6687f7
ceej <ceej_7@hotmail.com>
2019-10-08 00:56:00 +0800

    test: publishAnArticle单元测试

--- src/test/java/controller/ArticleControllerTest.java
+++ src/test/java/controller/ArticleControllerTest.java
@@ -3,17 +3,53 @@ package controller;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ArticleControllerTest {
     DataBaseUtility mock_dbUtil;
-
-    @BeforeAll
+    ArticleController articleController;
+    @BeforeEach
     public void mocksetup(){
         mock_dbUtil = mock(DataBaseUtility.class);
+        articleController = new ArticleController(mock_dbUtil);
     }
 
+    // testing for publishAnArticle
     @Test
-    @DisplayName("user")
+    @DisplayName("publishAnArticle: userID都不能为空")
+    public void publishAnArticle_userId_should_not_be_null(){
+        when(mock_dbUtil.isUserExisted(null)).thenReturn(false);
+        assertEquals(false, articleController.publishAnArticle(null,"content","."));
+    }
+    @Test
+    @DisplayName("publishAnArticle: content都不能为空")
+    public void publishAnArticle_content_should_not_be_null(){
+        when(mock_dbUtil.isUserExisted(null)).thenReturn(false);
+        when(mock_dbUtil.addArticle("1",null,".")).thenReturn(false);
+        assertEquals(false, articleController.publishAnArticle("1",null,"."));
+    }
+    @Test
+    @DisplayName("publishAnArticle: imgUrl都不能为空")
+    public void publishAnArticle_imgUrl_should_not_be_null(){
+        when(mock_dbUtil.isUserExisted(null)).thenReturn(false);
+        when(mock_dbUtil.addArticle("1","content",null)).thenReturn(false);
+        assertEquals(false, articleController.publishAnArticle("1","content",null));
+    }
+    @Test
+    @DisplayName("publishAnArticle: 发布的userid存在")
+    public void publishAnArticle_with_existed_user(){
+        when(mock_dbUtil.isUserExisted("1")).thenReturn(true);
+        when(mock_dbUtil.addArticle("1","content",".")).thenReturn(true);
+        assertEquals(true, articleController.publishAnArticle("1","content","."));
+    }
+    @Test
+    @DisplayName("publishAnArticle: 发布的userid不存在")
+    public void publishAnArticle_with_none_existed_user(){
+        when(mock_dbUtil.isUserExisted("1")).thenReturn(false);
+        when(mock_dbUtil.addArticle("1","content",".")).thenReturn(true);
+        assertEquals(false, articleController.publishAnArticle("1","content","."));
+    }
 
 }
