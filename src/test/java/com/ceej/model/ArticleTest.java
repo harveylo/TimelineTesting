94b2b2d383f8d2796a9ce6643b64c5465668e332
ceej <ceej_7@hotmail.com>
2019-10-31 16:47:35 +0800

    test: add refractored SpringMVC controller testing

--- src/test/java/com/ceej/model/ArticleTest.java
+++ src/test/java/com/ceej/model/ArticleTest.java
@@ -18,42 +18,42 @@ class ArticleTest {
     @Test
     @DisplayName("设置时间为now")
     public void set_article_to_now(){
-        article.setTimeStamp("2019-10-08 14:50:00");
+        article.setTimeStamp("2019-10-31 14:32:00.0");
         assertEquals("刚刚", article.getTimeStamp());
     }
 
     @Test
     @DisplayName("设置时间为5分钟前")
     public void set_article_to_five_min_before(){
-        article.setTimeStamp("2019-10-08 14:45:00");
+        article.setTimeStamp("2019-10-31 14:31:00.0");
         assertEquals("5分钟前", article.getTimeStamp());
     }
 
     @Test
     @DisplayName("设置时间为5小时前")
     public void set_article_to_five_hours_before(){
-        article.setTimeStamp("2019-10-08 09:40:00");
+        article.setTimeStamp("2019-10-31 09:44:00.0");
         assertEquals("5小时前", article.getTimeStamp());
     }
 
     @Test
     @DisplayName("设置时间为一天前")
     public void set_article_to_one_day_before(){
-        article.setTimeStamp("2019-10-07 09:40:00");
-        assertEquals("昨天 09:40", article.getTimeStamp());
+        article.setTimeStamp("2019-10-30 14:31:00.0");
+        assertEquals("昨天 14:31", article.getTimeStamp());
     }
 
     @Test
     @DisplayName("设置时间为一个月前")
     public void set_article_to_one_month_before(){
-        article.setTimeStamp("2019-09-08 09:40:00");
-        assertEquals("9月8日9时", article.getTimeStamp());
+        article.setTimeStamp("2019-09-30 14:31:00.0");
+        assertEquals("9月30日14时", article.getTimeStamp());
     }
 
     @Test
     @DisplayName("设置时间为一年前")
     public void set_article_to_one_year_before(){
-        article.setTimeStamp("2018-09-08 09:40:00");
-        assertEquals("2018年9月8日", article.getTimeStamp());
+        article.setTimeStamp("2018-09-30 14:31:00.0");
+        assertEquals("2018年9月30日", article.getTimeStamp());
     }
 }
