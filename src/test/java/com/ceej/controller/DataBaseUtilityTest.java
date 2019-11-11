52302cad1ccf75c0ee579a6c4b593b3e41bc4b51
mikotokirino <shikirin556@gmail.com>
2019-11-11 16:08:39 +0800

    check the add user function, has 1 test fail

--- src/test/java/com/ceej/controller/DataBaseUtilityTest.java
+++ src/test/java/com/ceej/controller/DataBaseUtilityTest.java
@@ -1,5 +1,6 @@
 package com.ceej.controller;
 
+import com.ceej.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -42,12 +43,40 @@ class DataBaseUtilityTest {
 
         ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
 
-        verify(statement).setString(eq(1),stringArgumentCaptor.capture());
+        verify(statement).setString(eq(1), stringArgumentCaptor.capture());
 
-        assertEquals("1",stringArgumentCaptor.getAllValues().get(0));
+        assertEquals("1", stringArgumentCaptor.getAllValues().get(0));
 
 //        verify(statement).executeUpdate();
 //        verify(connection).commit();
+        verify(statement).close();
+        verify(connection).close();
+    }
+
+    @Test
+    void add_user() throws SQLException {
+        User user = new User();
+        user.setNickname("test1");
+        user.setPassword("123456");
+        user.setUserID("42");
+
+        when(connection.prepareStatement(anyString())).thenReturn(statement);
+
+        boolean success = dao.addUser(user.getUserID(), user.getNickname(), user.getPassword());
+        assertTrue(success);
+
+        ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> integerArgumentCaptor = ArgumentCaptor.forClass(Integer.class);
+
+        verify(statement, times(4)).setString(integerArgumentCaptor.capture(), stringArgumentCaptor.capture());
+
+        assertEquals("42",stringArgumentCaptor.getAllValues().get(1));
+        assertEquals("test1",stringArgumentCaptor.getAllValues().get(2));
+        assertEquals("123456",stringArgumentCaptor.getAllValues().get(3));
+
+        verify(statement).executeUpdate();
+//        这个可以不调用吗 不提交的话好像是不会修改数据库的吧
+        verify(connection).commit();
         verify(statement).close();
         verify(connection).close();
     }
